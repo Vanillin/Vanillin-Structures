b53839f73ff8959968f72de6cb589bf79eed94f4
Vanillin <ivan.elistrat@mail.ru>
2024-10-28 20:49:16 +0300

    Added new project MultiThreading

---
+++ MultiThreading/MyByte.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace MultiThreading
+{
+    public readonly struct MyByte
+    {
+        private readonly int[] Bytes;
+        public MyByte(Byte b)
+        {
+            int ToInt = (int)b;
+            int[] MemoryBytes = new int[8];
+            int i = 0;
+            while (ToInt != 0)
+            {
+                MemoryBytes[i] = ToInt % 2;
+                ToInt /= 2;
+                i++;
+            }
+
+            Bytes = new int[i];
+            for (int j = 0; j < i; j++)
+            {
+                Bytes[j] = MemoryBytes[j];
+            }
+        }
+        private MyByte(int[] Bytes)
+        {
+            foreach(int b in Bytes)
+            {
+                if (b != 0 && b != 1) throw new ArgumentException("Неверно задан MyByte");
+            }
+            this.Bytes = Bytes;
+        }
+        public bool IsNull()
+        {
+            if (Bytes == null || Bytes.Length == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static MyByte operator +(MyByte a, MyByte b)
+        {
+            int[] retur;
+            int count = Math.Max(a.Bytes.Length, b.Bytes.Length);
+            int[] NewBytes = new int[count];
+            int memory = 0;
+            for (int i = 0; i < count; i++)
+            {
+                int sum = memory;
+                if (i < a.Bytes.Length)
+                {
+                    sum += a.Bytes[i];
+                }
+                if (i < b.Bytes.Length)
+                {
+                    sum += b.Bytes[i];
+                }
+                NewBytes[i] = sum % 2;
+                memory = sum / 2;
+            }
+            if (memory == 1)
+            {
+                retur = new int[count + 1];
+                for (int i = 0; i < count; i++)
+                {
+                    retur[i] = NewBytes[i];
+                }
+                retur[count] = 1;
+            }
+            else
+            {
+                retur = NewBytes;
+            }
+            return new MyByte(retur);
+        }
+        public override string ToString()
+        {
+            string ret = "";
+            for (int i = 0; i < Bytes.Length; i++)
+            {
+                ret = $"{Bytes[i]}{ret}";
+            }
+            return ret;
+        }
+
+    }
+}
---
+++ MultiThreading/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    static class Program
+    {
+        static Queue<string> Paths = new Queue<string>();
+        static ReaderWriterLock RWLock = new ReaderWriterLock();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь папки, файлы которой будут анализироваться: ");
+            string PathFile = Console.ReadLine();
+            bool ReadNow = true;
+
+            #region TaskCalculate
+            Task CalculateFile = Task.Run(() =>
+            {
+                while (Paths.Count != 0 || ReadNow)
+                {
+                    if (Paths.Count != 0)
+                    {
+                        RWLock.AcquireWriterLock(100);    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                        FileInfo f = new FileInfo(Paths.Dequeue());
+                        Console.WriteLine($"  Анализ файла: {f.FullName}");
+                        RWLock.ReleaseWriterLock();
+                        MyByte Sum = new MyByte(0);
+                        byte[] ar = File.ReadAllBytes(f.FullName);
+                        foreach (byte a in ar)
+                        {
+                            if (a % 2 != 0)
+                            {
+                                Sum += new MyByte(a);
+                            }
+                        }
+                        Console.WriteLine($"  Анализ файла пройден: {Sum.ToString()} <- {f.FullName}");
+                    }
+                }
+            });
+            #endregion
+            #region TaskRead
+            Task ReadFile = Task.Run(() =>
+            {
+                Queue<string> qu = new Queue<string>();
+                qu.Enqueue(PathFile);
+                while (qu.Count != 0)
+                {
+                    string folder = qu.Dequeue();
+                    foreach (string str in Directory.GetDirectories(folder))
+                    {
+                        qu.Enqueue(str);
+                    }
+                    foreach (string str in Directory.GetFiles(folder))
+                    {
+                        if (File.Exists(str))
+                        {
+                            RWLock.AcquireReaderLock(100);    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                            Paths.Enqueue(new FileInfo(str).FullName);
+                            Console.WriteLine($"  Файл добавлен в очередь: {new FileInfo(str).FullName}");
+                            RWLock.ReleaseReaderLock();
+                        }
+                    }
+                }
+                ReadNow = false;
+            });
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
